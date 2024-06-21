78e900655428df3213f0a636676389bf3175e76d
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2024-06-21 20:48:14 +0400

    navbar and mobile navbar finished

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image"
+import Link from "next/link"
 import React from "react"
+import MobileNav from "./MobileNav"
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="logo"
+          className="max-sm:size-10"
+        />
+        <p className="select-none text-[26px] font-extrabold max-sm:hidden text-white">
+          Pixel <span className="text-[#2181f9]">Talk</span>
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        {/* clerk user mangment */}
+        <MobileNav />
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
