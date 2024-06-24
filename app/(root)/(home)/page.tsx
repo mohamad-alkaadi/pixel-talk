a4d353088dd1c3a9ee955dd046f65e7bae1f1e69
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2024-06-24 09:38:15 +0400

    starting home

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,13 @@ import React from "react"
 const Home = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Home</h1>
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meting at: 12:30 PM
+          </h2>
+        </div>
+      </div>
     </section>
   )
 }
